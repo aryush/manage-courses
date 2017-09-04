df3bdfc510629ed8f8fd3994d0aa15a4758e666c
Aayush <superkool00@gmail.com>
2017-09-04 19:56:24 +0530

    adding change handler

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -13,6 +13,15 @@ class ManageCoursePage extends Component {
             course: Object.assign({}, this.props.course),
             errors: {}
         };
+
+        this.updateCourseState = this.updateCourseState.bind(this);
+    }
+
+    updateCourseState(event) {
+        const field = event.target.name;
+        let course = Object.assign({}, this.state.course);
+        course[field] = event.target.value;
+        return this.setState({course: course});
     }
 
     render() {
@@ -21,6 +30,7 @@ class ManageCoursePage extends Component {
                 <h1>Manage Course</h1>
                 <CourseForm
                     allAuthors={this.props.authors}
+                    onChange = {this.updateCourseState}
                     course={this.state.course}
                     errors={this.state.errors}
                 />
